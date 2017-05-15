3afd5f06d009fa5939f7cbd934133909509155a0
Pudding <aideen@aideen.pw>
2017-05-15 02:11:30 -0700

    Optimize startup and close properly

--- index.js
+++ index.js
@@ -2,37 +2,46 @@ const {app, BrowserWindow} = require('electron')
 const url = require('url')
 const path = require('path')
 
-if(app.makeSingleInstance((argv, workingDirectory) => {
-  if(!(global.tisshubox instanceof BrowserWindow)) return
+global.tisshubox = true
+if(app.makeSingleInstance(relaunch)) return app.quit()
+
+app.once('ready', async () => {
+  try {
+    require('electron-devtools-installer').default('nhdogjmejiglipccpnnnanhbledajbpd')
+    global.debug = true
+  } catch(e) {
+    global.debug = false
+  }
+
+  makeTisshubox()
+})
+
+function makeTisshubox() {
+  tisshubox = new BrowserWindow({
+    width: 1280, height: 720,
+    useContentSize: true,
+    title: 'Tisshubox',
+    webPreferences: {
+      devTools: global.debug,
+      textAreasAreResizable: false,
+      defaultEncoding: 'utf8'
+    }
+  })
+  tisshubox.loadURL(`file://${__dirname}/public/index.html`)
+  tisshubox.once('closed', () => {
+    tisshubox = null
+    if(process.platform !== 'darwin') app.quit()
+  })
+}
+
+app.on('open-file', relaunch)
+app.on('open-url', relaunch)
+function relaunch(event) {
+  if(typeof event.preventDefault === 'function') event.preventDefault()
+  if(tisshubox === true) return
+  if(tisshubox === null) return makeTisshubox()
+
   tisshubox.show()
   tisshubox.restore()
   tisshubox.focus()
-})) {
-  app.quit()
-} else {
-  app.once('ready', async () => {
-    try {
-      await require('electron-devtools-installer').default('nhdogjmejiglipccpnnnanhbledajbpd')
-      global.debug = true
-    } catch(e) {
-      global.debug = false
-    }
-
-    let tisshubox = new BrowserWindow({
-      width: 1280, height: 720,
-      useContentSize: true,
-      title: 'Tisshubox',
-      show: false,
-      webPreferences: {
-        devTools: global.debug,
-        textAreasAreResizable: false,
-        defaultEncoding: 'utf8'
-      }
-    })
-    tisshubox.loadURL(`file://${__dirname}/public/index.html`)
-    tisshubox.on('ready-to-show', () => {
-      global.tisshubox = tisshubox
-      tisshubox.show()
-    })
-  })
 }
