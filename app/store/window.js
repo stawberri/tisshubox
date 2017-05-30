8828e8b27f652c843775b488a792b40ab91e3958
Pudding <aideen@aideen.pw>
2017-05-30 12:36:22 -0700

    Unseen image count in window title

--- app/store/window.js
+++ app/store/window.js
@@ -13,6 +13,25 @@ module.exports = store => {
       detected: true
     }),
 
+    getters: {
+      title() {
+        let {package, window: {title}, posts: {tisshus}} = store.state
+
+        if(package.github && package.local.version !== package.github.version) {
+          title = `Tisshubox v${package.local.version} — UPDATE AVAILABLE. PLEASE CHECK GITHUB FOR A NEWER VERSION.`
+        } else if(!title) {
+          title = `Tisshubox v${package.local.version} — ${package.local.description}`
+        } else {
+          title += ` — Tisshubox v${package.local.version}`
+        }
+
+        let unseen = tisshus.filter(tisshu => tisshu.ready && !tisshu.seen).length
+        if(unseen) title = `(${unseen}) ${title}`
+
+        return title
+      }
+    },
+
     mutations: {
       title(state, {title}) {
         state.title = title
@@ -28,18 +47,12 @@ module.exports = store => {
     }
   })
 
-  store.watch(() => store.state.window.title, title => {
-    let package = store.state.package
-    if(package.github && package.local.version !== package.github.version) {
-      title = `Tisshubox v${package.local.version} — UPDATE AVAILABLE. PLEASE CHECK GITHUB FOR A NEWER VERSION.`
-    } else if(!title) {
-      title = `Tisshubox v${package.local.version} — ${package.local.description}`
-    } else {
-      title += ` — Tisshubox v${package.local.version}`
-    }
+  store.watch(
+    () => store.getters['window/title'],
+    title => document.title = title,
+    {immediate: true}
+  )
 
-    document.title = title
-  })
 
   function updateWindowState() {
     store.commit('window/state', {data: {
